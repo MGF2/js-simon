15902b002f74c448cf6d001bdb83d377c64eab49
Unknown <fioremartina@hotmail.com>
2020-07-27 14:27:47 +0200

    without timer

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var numeriPc = [];
+
+while ( numeriPc.length < 5 ) {
+ var numero = RandomNum(1,100);
+ var presente = numeriPc.includes(numero);
+ if (presente == false) {
+   numeriPc.push(numero);
+ }
+};
+
+console.log(numeriPc);
+alert('Memorizza i numeri ' + numeriPc);
+
+var giocatore = [];
+var match = [];
+
+while ( giocatore.length < 5) {
+  var numeriGiocatore = parseInt(prompt('Inserisci un numero'));
+  var presente = giocatore.includes(numeriGiocatore);
+  var simon = numeriPc.includes(numeriGiocatore);
+  if (presente == false) {
+    giocatore.push(numeriGiocatore);
+  }
+  if (simon == true) {
+    match.push(numeriGiocatore);
+  }
+}
+
+console.log(giocatore);
+console.log('Hai indovinato ' + match.length + ' numeri! -> ' + match);
+console.log(match);
+
+
+
+// FUNZIONI
+function RandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min )
+};
